fce9049a8663778a7b3390e5aab9a44a136a03e5
Adity-17 <128704904+Adity-17@users.noreply.github.com>
2023-04-26 11:13:11 +0530

    Update Calculation.java

--- Calculation.java
+++ Calculation.java
@@ -5,7 +5,10 @@ public class Calculation {
      int a=10;
      int b=20;
      int c=a+b;
+     int d=a*b;
      System.out.println("the sum is ="  +  c);
+      System.out.println("the PRODUCT is ="  +  d);
+      
      
 
 	}
